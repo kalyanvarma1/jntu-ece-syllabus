199aa5269ca72d54b0e69d91dbd17be1d1950bb6
kalyan <111264371+kalyanvarma1@users.noreply.github.com>
2023-01-08 21:41:31 +0530

    add initial files

---
+++ index.js
@@ -0,0 +1,49 @@
+function myfunction(){
+window.open("https://www.youtube.com/watch?v=7jaa1rlW7Ak&list=PLBlnK6fEyqRiw-GZRqfnlVIBz9dxrqHJS","_blank");
+}
+function textbook(){
+window.open("https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjF7bOJpar8AhXw33MBHTxvCHYQFnoECA4QAQ&url=https%3A%2F%2Farchive.org%2Fdownload%2FElectronicDevicesAndCircuitTheory%2FElectronic%2520Devices%2520and%2520Circuit%2520Theory.pdf&usg=AOvVaw0FTxfwQTv6gVeDPKgsDmue","_blank");
+}
+function previous(){
+window.open("https://studentservices.jntuh.ac.in/oss/resources/files/previousQPapers/1.B.Tech/1.R18/3.II-I/4.ECE/2.ELECTRONIC%20DEVICES%20AND%20CIRCUITS/March-2022.pdf","_blank");
+}
+function myfunction1(){
+window.open("https://www.youtube.com/watch?v=7nRZY69RaUQ&list=PL5UavDcOIMMLcrFn1PqALX6Z6Kkq9TBSS&index=5","_blank");
+}
+function previous1(){
+window.open("https://studentservices.jntuh.ac.in/oss/resources/files/previousQPapers/1.B.Tech/1.R18/3.II-I/4.ECE/3.NETWORK%20ANALYSIS%20AND%20TRANSMISSION%20LINES/March-2022.pdf","_blank");
+}
+function textbook2(){
+window.open("https://www.google.co.in/books/edition/Signals_and_Systems/HY71AAAAQBAJ?hl=en&gbpv=1&printsec=frontcover","_blank");
+}
+function previous2(){
+window.open("https://studentservices.jntuh.ac.in/oss/resources/files/previousQPapers/1.B.Tech/1.R18/3.II-I/4.ECE/5.SIGNALS%20AND%20SYSTEMS/March-2022.pdf","_blank");
+}
+function myfunction2(){
+window.open("https://www.youtube.com/watch?v=s8rsR_TStaA&list=PLBlnK6fEyqRhG6s3jYIU48CqsT5cyiDTO","_blank");
+}
+function myfunction3(){
+window.open("https://www.youtube.com/watch?v=M0mx8S05v60&list=PLBlnK6fEyqRjMH3mWf6kwqiTbT798eAOm","_blank");
+}
+function previous3(){
+window.open("https://studentservices.jntuh.ac.in/oss/resources/files/previousQPapers/1.B.Tech/1.R18/3.II-I/4.ECE/1.DIGITAL%20SYSTEM%20DESIGN/March-2022.pdf","_blank");
+}
+function textbook3(){
+window.open("https://wp.kntu.ac.ir/dfard/ebook/lc_ds1/HillPeterson-IntroductionToSwitchingTheoryLogicalDesign.pdf","_blank");
+}
+function previous4(){
+window.open("https://studentservices.jntuh.ac.in/oss/resources/files/previousQPapers/1.B.Tech/1.R18/3.II-I/4.ECE/4.PROBABILITY%20THEORY%20AND%20STOCHASTIC%20PROCESSES/March-2022.pdf","_blank");
+}
+function opensyllabus(){
+window.open("syllabus/Electronics_Communication_Engineering.pdf","_blank");
+}
+function myfunction4(){
+window.open("https://www.youtube.com/watch?v=KcXCARql-6A&list=PLp6ek2hDcoNBtK-hVSSPlbBzfxXwWON4x","_blank");
+}
+function textbook4(){
+window.open("syllabus/PTSP 1 2 3.pdf","_blank");
+}
+function textbook1(){
+window.open("syllabus/NATL 5lsns.pdf","_blank");
+}
+
